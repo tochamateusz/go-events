f0f9a2323cbaef9b4c259494c129bd15ea1cbfe7
Tocha Mateusz <tocha.mateusz+main@gmail.com>
2024-04-12 20:58:31 +0200

    feat: subscribing topic

--- 03-message-broker/03-subscribing/main.go
+++ 03-message-broker/03-subscribing/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
+
+	logger := watermill.NewStdLogger(false, false)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	subscriber, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	messages, err := subscriber.Subscribe(context.Background(), "progress")
+	if err != nil {
+		panic(err)
+	}
+
+	for msg := range messages {
+		payload := string(msg.Payload)
+		fmt.Printf("Message ID: %s - %s \n", msg.UUID, payload+"%")
+		msg.Ack()
+	}
 
 }
